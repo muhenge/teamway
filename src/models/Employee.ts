0b0af2bd471ca78f5c748caa5271f4a5b2429ed4
NGENZI Herve <48619051+muhenge@users.noreply.github.com>
2024-02-08 13:33:29 +0200

    Set employee model in progress

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,1 +1,14 @@
+import { Schema, model, Document } from "mongoose";
+import { IShift } from "./Shift";
 
+export interface IEmployee extends Document {
+  name: string;
+  shifts: IShift[];
+}
+
+export const EmployeeSchema = new Schema<IEmployee>({
+  name: { type: String, required: true },
+  shifts: [{ type: Schema.Types.ObjectId, ref: 'Shift' }]
+}, {timestamps: true})
+
+export const Employee = model<IEmployee>('Employee', EmployeeSchema)
